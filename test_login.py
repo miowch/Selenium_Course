b342dd772032854766170a0e2e93b9167d9e8f0e
Nastia Kuzina <miowch@gmail.com>
2022-02-15 17:18:57 +0100

    Ex 3 Log in to Litecart

---
+++ test_login.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome("./chromedriver")
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_login(driver):
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element(By.NAME, "username").send_keys("admin")
+    driver.find_element(By.NAME, "password").send_keys("admin")
+    driver.find_element(By.NAME, "login").click()
+    WebDriverWait(driver, 10).until(EC.title_is("My Store"))
+
